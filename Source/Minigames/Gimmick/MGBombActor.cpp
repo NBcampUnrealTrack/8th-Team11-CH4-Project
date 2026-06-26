fe185b8c2e6dba12ab020383178d59a46a7d2bb4
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-06-26 19:52:45 +0900

    add : 폭탄돌리기 미니게임에 사용할 MGBombActor 생성

---
+++ Source/Minigames/Gimmick/MGBombActor.cpp
@@ -0,0 +1,24 @@
+﻿// MGBombActor.cpp
+
+#include "Gimmick/MGBombActor.h"
+
+#include "Component/MGStatusComponent.h"	// 데미지를 주기 위해
+
+AMGBombActor::AMGBombActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AMGBombActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AMGBombActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/Minigames/Gimmick/MGBombActor.h
@@ -0,0 +1,36 @@
+﻿// MGBombActor.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MGBombActor.generated.h"
+
+UCLASS()
+class MINIGAMES_API AMGBombActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AMGBombActor();
+
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	// BombMesh를 부착할 위치
+	UPROPERTY() 
+	USceneComponent* Root;
+
+	// BombActor의 Static Mesh
+	UPROPERTY() 
+	UStaticMeshComponent* BombMesh;
+
+	// BombActor를 다른 사람에게 부착하기 위해 Collision 감지용 Sphere Component
+	UPROPERTY() 
+	class USphereComponent* PassTrigger;
+
+
+};
