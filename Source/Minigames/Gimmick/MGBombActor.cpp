9070e157524e18c38d9f40ac8ca3da0f54a872a3
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-06-26 21:35:02 +0900

    feat : BombActor 기본 구현

--- Source/Minigames/Gimmick/MGBombActor.cpp
+++ Source/Minigames/Gimmick/MGBombActor.cpp
@@ -1,24 +1,74 @@
 ﻿// MGBombActor.cpp
 
 #include "Gimmick/MGBombActor.h"
+#include "Minigames.h"						// Log 매크로 
+#include "Component/MGStatusComponent.h"	// PlayerCharacter의 Status -> HP, Stamina 등 이용
+#include "Components/SphereComponent.h"		// 폭탄을 넘길 Sphere Collision
+#include "GameFramework/Character.h"		// Character
+#include "Net/UnrealNetwork.h"				// Replication
 
-#include "Component/MGStatusComponent.h"	// 데미지를 주기 위해
-
-AMGBombActor::AMGBombActor()
+AMGBombActor::AMGBombActor() :
+	PassTriggerRadius(100.f),
+	bCanPass(true),
+	PassCooldownTime(0.5f)
 {
 	PrimaryActorTick.bCanEverTick = false;
 
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RootComponent = Root;
+
+	BombMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BombStaticMesh"));
+	BombMesh->SetupAttachment(RootComponent);
+	BombMesh->SetCollisionProfileName(TEXT("NoCollision")); // 폭탄 자체는 NoCollision
+
+
+	PassTrigger = CreateDefaultSubobject<USphereComponent>(TEXT("PassTrigger"));
+	PassTrigger->SetupAttachment(RootComponent);
+	PassTrigger->SetSphereRadius(PassTriggerRadius);
+
+	bReplicates = true;
 }
 
 void AMGBombActor::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	// 서버에서만 충돌을 감지하도록 연결
+	if (HasAuthority())
+	{
+		PassTrigger->OnComponentBeginOverlap.AddDynamic(this, &AMGBombActor::OnTriggerOverlap);
+	}
 }
 
-void AMGBombActor::Tick(float DeltaTime)
+void AMGBombActor::OnTriggerOverlap(
+	UPrimitiveComponent* OverlappedComponent, 
+	AActor* OtherActor, 
+	UPrimitiveComponent* OtherComp, 
+	int32 OtherBodyIndex, bool bFromSweep, 
+	const FHitResult& SweepResult)
 {
-	Super::Tick(DeltaTime);
-
+	MG_LOG_ROLE(LogMGNet, Warning, TEXT(" Overlap Detected : %s"), *OtherActor->GetName());
 }
 
+void AMGBombActor::OnRep_BombHolder()
+{
+}
+
+void AMGBombActor::SetBombHolder(ACharacter* NewHolder)
+{
+}
+
+void AMGBombActor::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ThisClass, BombHolder);
+}
+
+void AMGBombActor::AttachToCurrentHolder()
+{
+}
+
+void AMGBombActor::ResetPassCooldown()
+{
+}
--- Source/Minigames/Gimmick/MGBombActor.h
+++ Source/Minigames/Gimmick/MGBombActor.h
@@ -14,10 +14,28 @@ class MINIGAMES_API AMGBombActor : public AActor
 public:	
 	AMGBombActor();
 
-	virtual void Tick(float DeltaTime) override;
+	// virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	void OnRep_BombHolder();
+
+
+	void SetBombHolder(ACharacter* NewHolder);
+
+	// Replication
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
 protected:
 	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnTriggerOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+private:
+	void AttachToCurrentHolder();
+
+	// Pass
+	void ResetPassCooldown();
 
 public:
 	// BombMesh를 부착할 위치
@@ -25,12 +43,28 @@ public:
 	USceneComponent* Root;
 
 	// BombActor의 Static Mesh
-	UPROPERTY() 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bomb")
 	UStaticMeshComponent* BombMesh;
 
 	// BombActor를 다른 사람에게 부착하기 위해 Collision 감지용 Sphere Component
 	UPROPERTY() 
 	class USphereComponent* PassTrigger;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bomb")
+	float PassTriggerRadius;
 
+	//----------------------------------------------------------
+	// 폭탄을 소유한 Character
+	UPROPERTY(ReplicatedUsing = OnRep_BombHolder)
+	ACharacter* BombHolder;
+
+private:
+	bool bCanPass;
+
+	// 다시 Pass에 필요한 쿨타임 관리 Timer
+	FTimerHandle PassCooldownTimer;
+
+	// 다시 Pass하기 위한 시간 값
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bomb")
+	float PassCooldownTime;
 };
