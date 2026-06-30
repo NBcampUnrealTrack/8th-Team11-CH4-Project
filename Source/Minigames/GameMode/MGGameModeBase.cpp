e7ea63a9787f376fb3c0325a20fde8de95942935
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-06-30 19:54:13 +0900

    feat : MGGameModeBase.h
    - PreLogin을 오버라이드 하여 Lobby 이외의 State에서 접속 차단
    - GIveScore() : Divide by 0 방지
    - OnMainTimerElapsed() : switch 분기 중
    case EMatchState::Ending 에서 심리스 트래블을 고려하여 내용 구현

--- Source/Minigames/GameMode/MGGameModeBase.cpp
+++ Source/Minigames/GameMode/MGGameModeBase.cpp
@@ -7,6 +7,7 @@
 #include "GameState/MGGameStateBase.h"
 #include "Kismet/GameplayStatics.h"
 #include "PlayerState/MGFlagPlayerState.h"
+#include "GameInstance/MGGameInstance.h"				// GameInstance
 
 AMGGameModeBase::AMGGameModeBase()
 {
@@ -16,6 +17,28 @@ AMGGameModeBase::AMGGameModeBase()
 	// 필요에 따라서는 PlayerCharacter도 유지
 }
 
+void AMGGameModeBase::PreLogin(const FString& Options, const FString& Address, const FUniqueNetIdRepl& UniqueId, FString& ErrorMessage)
+{
+	Super::PreLogin(Options, Address, UniqueId, ErrorMessage);
+
+	// GameInstance를 가져와서 현재 전체 세션의 라운드 상태를 확인
+	UMGGameInstance* MGGameInstance = Cast<UMGGameInstance>(GetGameInstance());
+
+	if (IsValid(MGGameInstance))
+	{
+		// 로비 상태가 아니라면 (Round1, Round2, Round3, FinalResult) 접속 차단
+		if (MGGameInstance->CurrentRoundState != ERoundState::Lobby)
+		{
+			ErrorMessage = TEXT("The tournament has already started. You can only join in the Lobby.");
+			// 에러 메세지에 유효한 값이 있으면 접속 차단
+			return;
+		}
+	}
+
+	// TODO : 만약 미니게임 도중에 접속을 허용하려면 로직을 변경해야함
+	// ex) Round2에서 재접속 시, 현재 미니게임에선 관전자로 전환하고 Round3에선 정상 플레이 가능
+}
+
 void AMGGameModeBase::PostLogin(APlayerController* NewPlayer)
 {
 	Super::PostLogin(NewPlayer);
@@ -23,12 +46,6 @@ void AMGGameModeBase::PostLogin(APlayerController* NewPlayer)
 	AMGGameStateBase* MGGameState = GetGameState<AMGGameStateBase>();
 	if (IsValid(MGGameState) == false)
 	{
-		return;
-	}
-
-	if (MGGameState->MatchState != EMatchState::Waiting)
-	{
-		NewPlayer->SetLifeSpan(0.1f);
 		return;
 	}
 
@@ -61,6 +78,10 @@ void AMGGameModeBase::BeginPlay()
 	RemainWaitingTimeForPlaying = WaitingTime;
 
 	RemainWaitingTimeForEnding = EndingTime;
+
+	// 30초 후 SeamlessTravel 하는지 테스트용
+	FTimerHandle TestEndTimerHandle;
+	GetWorld()->GetTimerManager().SetTimer(TestEndTimerHandle, this, &ThisClass::EndMinigame, 30.f, false);
 }
 
 void AMGGameModeBase::StartMinigame()
@@ -84,7 +105,7 @@ void AMGGameModeBase::OnCharacterDead(AMGPlayerController* InController)
 
 	InController->ClientRPCShowGameResultWidget(AllPlayerControllers.Num());
 
-	AllPlayerControllers.Remove(InController);
+	// AllPlayerControllers.Remove(InController);
 }
 
 void AMGGameModeBase::GiveScore(AMGPlayerState* PS, int32 Rank)
@@ -92,7 +113,8 @@ void AMGGameModeBase::GiveScore(AMGPlayerState* PS, int32 Rank)
 	int32 PlayerCount = GameState->PlayerArray.Num();
 
 	int32 AddScore = 0;
-	if (Rank == 1)
+
+	if (Rank == 1 || PlayerCount <= 1)
 	{
 		AddScore = 10;
 	}
@@ -146,23 +168,61 @@ void AMGGameModeBase::OnMainTimerElapsed()
 	}
 	case EMatchState::Ending:
 	{
-		FString NotificationString = FString::Printf(TEXT("Waiting %d for returning to title."), RemainWaitingTimeForEnding);
-
+		// 남은 시간 알림
+		FString NotificationString = FString::Printf(TEXT("Moving to next stage in %d seconds..."), RemainWaitingTimeForEnding);
 		NotifyToAllPlayer(NotificationString);
 
 		--RemainWaitingTimeForEnding;
 
+		// 카운트다운 종료 시 맵 이동
 		if (RemainWaitingTimeForEnding <= 0)
 		{
-			for (auto MGPC : AllPlayerControllers)
-			{
-				MGPC->ClientRPCReturnToTitle();
-			}
-
 			MainTimerHandle.Invalidate();
 
-			FName CurrentLevelName = FName(UGameplayStatics::GetCurrentLevelName(this));
-			UGameplayStatics::OpenLevel(this, CurrentLevelName, true, FString(TEXT("listen")));
+			// 심리스 트래블을 하더라도 초기화 되지 않는 GameInstance에 저장되어있는 현재 라운드 정보를 가져옴
+			UMGGameInstance* MGGameInstance = Cast<UMGGameInstance>(GetGameInstance());
+			FString NextMapURL = TEXT("");
+
+			if (IsValid(MGGameInstance))
+			{
+				// CurrentRoundState에 따라 다음 맵과 다음 라운드 상태를 갱신
+				switch (MGGameInstance->CurrentRoundState)
+				{
+				case ERoundState::Lobby:
+					NextMapURL = TEXT("/Game/Minigames/Level/L_MG_02_Flag");
+					// TODO : Lobby 추가 후 /Game/Minigames/Level/L_MG_01_BombTag 로 바꿔야함
+					MGGameInstance->CurrentRoundState = ERoundState::Round1; // 다음 상태로 미리 갱신
+					break;
+
+				case ERoundState::Round1:
+					NextMapURL = TEXT("/Game/Minigames/Level/L_MG_02_Flag");
+					MGGameInstance->CurrentRoundState = ERoundState::Round2;
+					break;
+
+				case ERoundState::Round2:
+					NextMapURL = TEXT("/Game/Minigames/Level/L_MG_03_XXX");
+					MGGameInstance->CurrentRoundState = ERoundState::Round3;
+					break;
+
+				case ERoundState::Round3:
+					// FinalResult을 별도 맵에 진행할거면 그곳으로, 아니라면 바로 로비로 이동(현재)
+					NextMapURL = TEXT("/Game/Level/Lobby");
+					MGGameInstance->CurrentRoundState = ERoundState::FinalResult;
+					break;
+
+				case ERoundState::FinalResult:
+					// 결과창에서 로비로 완전히 돌아가는 처리
+					NextMapURL = TEXT("/Game/Level/Lobby");
+					MGGameInstance->CurrentRoundState = ERoundState::Lobby;
+					break;
+				}
+			}
+
+			// 심리스 트래블 실행 (클라이언트들은 자동으로 서버를 따라옴)
+			if (NextMapURL.IsEmpty() == false)
+			{
+				GetWorld()->ServerTravel(NextMapURL);
+			}
 
 			return;
 		}
