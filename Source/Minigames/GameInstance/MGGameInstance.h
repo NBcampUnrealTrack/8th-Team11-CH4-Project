8df45ca8d1b47107ca910718c5d8a0341a274855
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-07-01 21:19:07 +0900

    chore : MGGameInstance.h
    cpp에서 구현부를 주석 처리했던 Init() 함수를 헤더에도 주석 처리

--- Source/Minigames/GameInstance/MGGameInstance.h
+++ Source/Minigames/GameInstance/MGGameInstance.h
@@ -13,7 +13,7 @@ class MINIGAMES_API UMGGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
 public:
-	virtual void Init() override;
+	// virtual void Init() override;
 	// 레벨 이동 시 저장해야할 데이터 임시로 보관
 public:
 
