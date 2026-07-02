dfe1ba1d6d12d3e87799d98423e33c1f6efaa61a
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-07-02 21:13:22 +0900

    delete : MGGameInstance.h
    필요 없는 주석 처리된 함수 삭제

--- Source/Minigames/GameInstance/MGGameInstance.h
+++ Source/Minigames/GameInstance/MGGameInstance.h
@@ -12,15 +12,11 @@ UCLASS()
 class MINIGAMES_API UMGGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
-public:
-	// virtual void Init() override;
-	// 레벨 이동 시 저장해야할 데이터 임시로 보관
+
 public:
 
 	UMGGameInstance();
 
-	//virtual void Init() override;
-	
 	UFUNCTION(BlueprintPure, Category = "Game Flow")
 	FString GetLevelURLForRound(int32 RoundIndex) const;
 
