9f1bae740d06c406cef005392cb30d008d90bd40
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-07-04 00:32:42 +0900

    add : MGEffectDataAsset.h/cpp
    프로젝트 전반에서 사용할 Effect(이동속도 증가/감소 등)의 DataAsset 클래스 생성

---
+++ Source/Minigames/Data/MGEffectDataAsset.h
@@ -0,0 +1,14 @@
+// MGEffectDataAsset.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "MGEffectDataAsset.generated.h"
+
+UCLASS()
+class MINIGAMES_API UMGEffectDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
