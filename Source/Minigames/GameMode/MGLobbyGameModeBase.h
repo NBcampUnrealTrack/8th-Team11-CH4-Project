8a85a9a9a7b0c3ba9f54f642262304814fb2dde8
YH760 <opqwoiwr@gmail.com>
2026-07-03 22:26:50 +0900

    add: 로비 플레이어 색 랜덤배정/변경/유지 시스템
    - 로비 접속 시 자동 랜덤배정, 로비 내 변경(콘솔 테스트용 ChangeColor), 미니게임 진입 후 잠금
    - 로비↔미니게임 경계(로비→Round1, Round3→로비 복귀)는 GameInstance로 색 유지
    - Round1→2→3 사이는 CopyProperties로 색 유지
    - AMGPlayerState.PlayerColor 리플리케이션 누락 수정
    - CopyProperties가 MGScore 대신 TotalScore를 복사하도록 수정 (기존 버그)

--- Source/Minigames/GameMode/MGLobbyGameModeBase.h
+++ Source/Minigames/GameMode/MGLobbyGameModeBase.h
@@ -6,7 +6,9 @@
 #include "GameFramework/GameModeBase.h"
 #include "MGLobbyGameModeBase.generated.h"
 
+enum class EMGPlayerColor : uint8;
 class AMGPlayerController;
+class AMGLobbyPlayerState;
 /**
  * 
  */
@@ -20,18 +22,26 @@ public:
 	
 	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 	
+	virtual void PreLogin(const FString& Options, const FString& Address,
+		const FUniqueNetIdRepl& UniqueId, FString& ErrorMessage) override;
+	
 	virtual void PostLogin(APlayerController* NewPlayer) override;
 	
 	virtual void Logout(AController* ExitingController) override;
 	
-	void OnPlayerReady(AMGPlayerController* PC, bool bReady);
-
 	// 미니게임 종료 후 Seamless를 통해 돌아오는 플레이어들을 처리하기 위한 함수 
 	virtual void HandleSeamlessTravelPlayer(AController*& C) override;
+	
+	void OnPlayerReady(AMGPlayerController* PC, bool bReady);
+	
+	void OnPlayerChangeColor(AMGPlayerController* PC, EMGPlayerColor NewColor);
 	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	int32 MinimumPlayerCount = 2;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int32 MaxPlayerCount = 10;
+	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	int32 CountdownTime = 10;
 
@@ -57,8 +67,11 @@ private:
 	
 	void GenerateMinigameSequence();
 	
+	void AssignRandomColorToPlayer(AMGLobbyPlayerState* PS);
+	
 	bool CheckAllPlayersReady();
 	
 	FTimerHandle CountdownTimerHandle;
-
+	
+	TArray<EMGPlayerColor> AvailableColors;
 };
