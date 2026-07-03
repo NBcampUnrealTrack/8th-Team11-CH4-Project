3f2dcbacf05715a0d6e43798f55a460de055cf50
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-07-04 00:46:59 +0900

    feat : MGEffectDataAsset.h
    MGEffectDataAsset의 데이터 구조 추가

--- Source/Minigames/Data/MGEffectDataAsset.h
+++ Source/Minigames/Data/MGEffectDataAsset.h
@@ -11,4 +11,28 @@ class MINIGAMES_API UMGEffectDataAsset : public UDataAsset
 {
 	GENERATED_BODY()
 	
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Effect|Name")
+	FName EffectID = NAME_None;
+
+	//-----------------------------
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Effect|Logic")
+	float SpeedAmount = 0.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Effect|Logic")
+	float Duration = 0.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Effect|Logic")
+	bool bCanStack = false;
+
+	//-----------------------------
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Effect|Visual")
+	FText DisplayName;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Effect|Visual")
+	class UTexture2D* BuffIcon;
+
+	// TODO : Niagara Effect, Sound µî
 };
