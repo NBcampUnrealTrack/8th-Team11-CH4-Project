cbb162b76ca052159c9d5ec20ccae58cc4a830a8
khalicika-dev <khalicika@gmail.com>
2026-07-05 07:58:29 +0900

    chore: 주석 추가, Fix: 탈락 시 캡슐 콜리전 제거

--- Source/Minigames/PlayerState/MGPassBombPlayerState.cpp
+++ Source/Minigames/PlayerState/MGPassBombPlayerState.cpp
@@ -25,14 +25,19 @@ void AMGPassBombPlayerState::MulticastRPC_RetireCharacter_Implementation()
 
 	if (IsValid(MGPC))
 	{
+		// 탈락 로그 출력
 		MG_LOG_NET(LogMGNet, Log, TEXT("%s has retired."), *MGPC->GetName());
+
+		// 탈락 대상자의 탈락효과 부여 (래그돌화, 폭발로 위로 튀어오름)
 		MGPC->GetMesh()->SetCollisionProfileName(TEXT("Ragdoll"));
 		MGPC->GetMesh()->SetSimulatePhysics(true);
 		MGPC->GetMesh()->AddImpulse(FVector(0, 0, 10000.f),NAME_None,true);
-		MGPC->GetCapsuleComponent()->Deactivate();
+		MGPC->GetCapsuleComponent()->SetCollisionProfileName(TEXT("NoCollision"));
 
-		if (IsValid(GetPlayerController()))	// 탈락 대상자라면
+		// 탈락 대상자의 컨트롤러
+		if (IsValid(GetPlayerController()))
 		{
+			// 캐릭터의 입력을 막고 관찰자 모드로 전환
 			MGPC->DisableInput(GetPlayerController());
 			MG_LOG_NET(LogMGNet, Log, TEXT("SpectatorPawnName: %s"), *Spectator->GetName());
 			Spectator->DeathCamFollowCharacter(GetPlayerController(), MGPC, 3.f);
