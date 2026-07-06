fe447ca3d36c62458124871c938555a0fb42c363
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-07-06 18:13:24 +0900

    add : UW_PassBombLayout.h/cpp
    현재 폭탄을 들고 있는 캐릭터 BombHolder가 누구인지를 보여주는 UserWidget 생성

---
+++ Source/Minigames/UI/UW_PassBombLayout.cpp
@@ -0,0 +1,13 @@
+﻿// UW_PassBombLayout.cpp
+
+#include "UI/UW_PassBombLayout.h"
+#include "Components/TextBlock.h"
+
+void UUW_PassBombLayout::UpdateBombHolderText(const FString& NewHolderName)
+{
+	if (IsValid(BombHolderText))
+	{
+		// 받아온 문자열을 화면 텍스트에 적용
+		BombHolderText->SetText(FText::FromString(NewHolderName));
+	}
+}
