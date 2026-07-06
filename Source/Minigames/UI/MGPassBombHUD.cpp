16833d7d4a74827137bc9235f5b5b3561b7ddc20
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-07-06 18:20:35 +0900

    add : MGPassBombHUD.h/cpp
    - MGHUDBase를 상속하는 폭탄돌리기 미니게임에서 사용할 HUD 파생 클래스
    - 'UW_PassBombLayout'을 멤버 변수로 가지고 있음

---
+++ Source/Minigames/UI/MGPassBombHUD.cpp
@@ -0,0 +1,71 @@
+﻿// MGPassBombHUD.cpp
+
+#include "UI/MGPassBombHUD.h"
+#include "Blueprint/UserWidget.h"
+
+#include "UI/UW_PassBombLayout.h"
+#include "Gimmick/MGBombActor.h"
+
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/PlayerState.h" // 닉네임을 가져오기 위해 추가
+
+AMGPassBombHUD::AMGPassBombHUD() 
+//	: BombHolderText(NAME_None)
+{
+}
+
+void AMGPassBombHUD::BeginPlay()
+{
+	// 공통 UI ( 부모 클래스 MGHUDBase )
+	Super::BeginPlay();
+
+	if (!IsValid(BombWidgetClass))
+	{
+		return;
+	}
+
+	BombWidgetInstance = CreateWidget<UUserWidget>(GetWorld(), BombWidgetClass);
+
+	if (IsValid(BombWidgetInstance))
+	{
+		BombWidgetInstance->AddToViewport();
+	}
+
+	UUW_PassBombLayout* MyBombWidget = Cast<UUW_PassBombLayout>(BombWidgetInstance);
+
+	if (!IsValid(MyBombWidget))
+	{
+		return;
+	}
+
+	AMGBombActor* FoundBomb = Cast<AMGBombActor>(UGameplayStatics::GetActorOfClass(GetWorld(), AMGBombActor::StaticClass()));
+
+	if (IsValid(FoundBomb))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Success to find Bomb"));
+		FoundBomb->OnBombHolderChanged.AddUObject(this, &AMGPassBombHUD::OnBombHolderUpdated);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("Fail to find Bomb"));
+	}
+
+}
+
+// MGBombActor에 존재하는 델리게이트에 바인딩 되어있음
+void AMGPassBombHUD::OnBombHolderUpdated(ACharacter* NewHolder)
+{
+	UUW_PassBombLayout* MyBombWidget = Cast<UUW_PassBombLayout>(BombWidgetInstance);
+	if (!IsValid(MyBombWidget))
+	{
+		return;
+	}
+
+	if (IsValid(NewHolder) && IsValid(NewHolder->GetPlayerState()))
+	{
+		FString PlayerName = NewHolder->GetPlayerState()->GetPlayerName();
+		MyBombWidget->UpdateBombHolderText(PlayerName);
+	}
+}
+
---
+++ Source/Minigames/UI/MGPassBombHUD.h
@@ -0,0 +1,33 @@
+﻿// MGPassBombHUD.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/MGHUDBase.h"
+#include "MGPassBombHUD.generated.h"
+
+class ACharacter;
+
+UCLASS()
+class MINIGAMES_API AMGPassBombHUD : public AMGHUDBase
+{
+	GENERATED_BODY()
+	
+public:
+	AMGPassBombHUD();
+
+protected:
+	virtual void BeginPlay() override;
+
+	void OnBombHolderUpdated(ACharacter* NewHolder);
+
+public:
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TSubclassOf<class UUserWidget> BombWidgetClass;
+
+private:
+	// 생성된 위젯을 메모리에 들고 있을 포인터 (가비지 컬렉션 방지)
+	UPROPERTY()
+	class UUserWidget* BombWidgetInstance;
+
+};
