3d96203bce4d806e52a8c8be6c536a38814dcdda
YH760 <opqwoiwr@gmail.com>
2026-07-07 20:30:19 +0900

    Feat: 깃발 미니게임 HUD 추가 (남은 시간/소지자 표시)
    - UW_FlagLayout 위젯 + MGFlagHUD 추가, HUD가 GameState를 구독해 UI 갱신
    - MGFlagGameStateBase에 RemainGameTime/CurrentFlagHolder 델리게이트(OnRep) 추가
    - 소유권 변경 시(SetHasFlag) GameState에 현재 소지자 등록
    - BP_MGFlagGameModeBase에 HUDClass(HUD_MGFlag) 연결

--- Source/Minigames/GameState/MGFlagGameStateBase.cpp
+++ Source/Minigames/GameState/MGFlagGameStateBase.cpp
@@ -2,13 +2,29 @@
 
 
 #include "GameState/MGFlagGameStateBase.h"
-
 #include "Net/UnrealNetwork.h"
 
 void AMGFlagGameStateBase::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 	
-	DOREPLIFETIME(ThisClass, RemainGameTime);
-
+	DOREPLIFETIME(AMGFlagGameStateBase, RemainGameTime);
+	DOREPLIFETIME(AMGFlagGameStateBase, CurrentFlagHolder);
 }
+
+void AMGFlagGameStateBase::SetCurrentFlagHolder(AMGFlagPlayerState* NewHolder)
+{
+	CurrentFlagHolder = NewHolder;
+	OnRep_CurrentFlagHolder(); // 리슨,로컬 대비
+}
+
+void AMGFlagGameStateBase::OnRep_RemainGameTime()
+{
+	OnRemainTimeChanged.Broadcast(RemainGameTime);
+}
+
+void AMGFlagGameStateBase::OnRep_CurrentFlagHolder()
+{
+	OnFlagHolderChanged.Broadcast(CurrentFlagHolder);
+}
+
--- Source/Minigames/GameState/MGFlagGameStateBase.h
+++ Source/Minigames/GameState/MGFlagGameStateBase.h
@@ -5,11 +5,12 @@
 #include "CoreMinimal.h"
 #include "MGGameStateBase.h"
 #include "GameFramework/GameStateBase.h"
+#include "PlayerState/MGFlagPlayerState.h"
 #include "MGFlagGameStateBase.generated.h"
 
-/**
- * 
- */
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnRemainTimeChanged, int32);
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnFlagHolderChanged, AMGFlagPlayerState*);
+
 UCLASS()
 class MINIGAMES_API AMGFlagGameStateBase : public AMGGameStateBase
 {
@@ -18,6 +19,22 @@ class MINIGAMES_API AMGFlagGameStateBase : public AMGGameStateBase
 public:
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 	
-	UPROPERTY(Replicated, VisibleAnywhere, BlueprintReadOnly)
+	void SetCurrentFlagHolder(AMGFlagPlayerState* NewHolder);
+	FORCEINLINE AMGFlagPlayerState* GetCurrentFlagHolder() const { return CurrentFlagHolder; }
+	
+	UPROPERTY(ReplicatedUsing = OnRep_RemainGameTime, VisibleAnywhere, BlueprintReadOnly)
 	int32 RemainGameTime;
+	
+	FOnRemainTimeChanged OnRemainTimeChanged;
+	FOnFlagHolderChanged OnFlagHolderChanged;
+	
+protected:
+	UFUNCTION()
+	void OnRep_RemainGameTime();
+	
+	UFUNCTION()
+	void OnRep_CurrentFlagHolder();
+	
+	UPROPERTY(ReplicatedUsing = OnRep_CurrentFlagHolder, VisibleAnywhere, BlueprintReadOnly)
+	AMGFlagPlayerState* CurrentFlagHolder = nullptr;
 };
