f39b7b24ce519381816e7d73a3bd4f7ea23eeef1
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-07-09 17:15:23 +0900

    feat : MGPlayerController.h
    컷신 TArray의 Getter 추가

--- Source/Minigames/Controller/MGPlayerController.h
+++ Source/Minigames/Controller/MGPlayerController.h
@@ -58,11 +58,6 @@ public:
 
 #pragma region CutScene
 
-protected:
-	// 블루프린트에서 만든 Level Sequence 에셋 포인터를 저장할 TArray
-	UPROPERTY(EditDefaultsOnly, Category = "Cinematic")
-	TArray<TObjectPtr<ULevelSequence>> CutSceneAssets;
-
 public:
 	// ClientRPC, 서버에서 모든 Client들에게 해당 함수를 실행하라고 명령
 	UFUNCTION(Client, Reliable)
@@ -70,6 +65,13 @@ public:
 
 	UFUNCTION()
 	void OnCutSceneFinished();
+
+	const TArray<TObjectPtr<ULevelSequence>>& GetCutSceneAssets() const { return CutSceneAssets; }
+
+protected:
+	// 블루프린트에서 만든 Level Sequence 에셋 포인터를 저장할 TArray
+	UPROPERTY(EditDefaultsOnly, Category = "Cinematic")
+	TArray<TObjectPtr<ULevelSequence>> CutSceneAssets;
 
 #pragma endregion
 
