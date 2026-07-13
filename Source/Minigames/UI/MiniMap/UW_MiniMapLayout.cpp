b783d518a4d18dd9951c71b7e418b13741f0535e
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-07-13 18:27:29 +0900

    add : UW_MiniMapLayout.h/cpp
    MGHUDBase 에서 사용할 미니맵 위젯

---
+++ Source/Minigames/UI/MiniMap/UW_MiniMapLayout.cpp
@@ -0,0 +1,65 @@
+﻿// UW_MiniMapLayout.cpp
+
+#include "UI/MiniMap/UW_MiniMapLayout.h"
+#include "Components/Image.h"
+#include "Components/CanvasPanel.h"
+#include "Components/CanvasPanelSlot.h"
+#include "Kismet/GameplayStatics.h"
+#include "UI/MiniMap/MGMiniMapCamera.h"
+
+void UUW_MiniMapLayout::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	// PlayerController가 소유중인 Pawn = 각자의 Pawn
+	CachedPlayerPawn = GetOwningPlayerPawn();
+
+	// 맵에 배치된 미니맵 카메라 찾기
+	CachedMinimapCamera = Cast<AMGMiniMapCamera>(UGameplayStatics::GetActorOfClass(GetWorld(), AMGMiniMapCamera::StaticClass()));
+
+	// 0.1초마다 UpdateIconPosition 함수를 실행하는 타이머 가동
+	if (IsValid(CachedPlayerPawn) && IsValid(CachedMinimapCamera))
+	{
+		GetWorld()->GetTimerManager().SetTimer(
+			UpdateTimerHandle,
+			this,
+			&UUW_MiniMapLayout::UpdateIconPosition,
+			0.1f,
+			true
+		);
+	}
+}
+
+void UUW_MiniMapLayout::NativeDestruct()
+{
+	// 위젯이 파괴될 때 안전하게 모든 Timer Clear
+	GetWorld()->GetTimerManager().ClearTimer(UpdateTimerHandle);
+
+	Super::NativeDestruct();
+}
+
+void UUW_MiniMapLayout::UpdateIconPosition()
+{
+	// 안전 검사
+	if (!IsValid(CachedPlayerPawn) || !IsValid(CachedMinimapCamera) || !IsValid(PlayerIcon))
+	{
+		return;
+	}
+
+	// 월드 좌표 가져오기
+	FVector PlayerLoc = CachedPlayerPawn->GetActorLocation();
+	FVector CameraLoc = CachedMinimapCamera->GetActorLocation();
+	float OrthoWidth = CachedMinimapCamera->OrthographicSize;
+
+	// 3D 월드 좌표 -> 2D UI 좌표 변환 공식 (언리얼 좌표계 기준)
+	// X축: 플레이어의 Y(좌우) - 카메라의 Y(좌우)
+	// Y축: 플레이어의 X(앞뒤) - 카메라의 X(앞뒤)에 마이너스 적용 (UI는 아래로 갈수록 +이므로)
+	float UI_X = ((PlayerLoc.Y - CameraLoc.Y) / OrthoWidth) * MinimapSize.X;
+	float UI_Y = (-(PlayerLoc.X - CameraLoc.X) / OrthoWidth) * MinimapSize.Y;
+
+	// 캔버스 슬롯으로 형변환하여 위치 적용
+	if (UCanvasPanelSlot* IconSlot = Cast<UCanvasPanelSlot>(PlayerIcon->Slot))
+	{
+		IconSlot->SetPosition(FVector2D(UI_X, UI_Y));
+	}
+}
---
+++ Source/Minigames/UI/MiniMap/UW_MiniMapLayout.h
@@ -0,0 +1,53 @@
+﻿// UW_MiniMapLayout.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UW_MiniMapLayout.generated.h"
+
+class APawn;
+class AMGMiniMapCamera;
+class UCanvasPanel;
+class UImage;
+
+UCLASS()
+class MINIGAMES_API UUW_MiniMapLayout : public UUserWidget
+{
+	GENERATED_BODY()
+
+#pragma region MiniMap
+
+protected:
+	// 위젯이 화면에 생성될 때 호출되는 초기화 함수
+	virtual void NativeConstruct() override;
+
+	// 위젯이 화면에서 사라질 때 호출되는 함수 (Destroy와 거의 유사, 주로 Timer 종료)
+	virtual void NativeDestruct() override;
+
+protected:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCanvasPanel> MinimapCanvas;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> PlayerIcon;
+
+	// 미니맵 위젯 크기
+	UPROPERTY(EditDefaultsOnly, Category = "Minimap")
+	FVector2D MinimapSize = FVector2D(256.0f, 256.0f);
+
+private:
+	// 타이머가 0.1초마다 호출할 플레이어 위치 갱신 함수
+	UFUNCTION()
+	void UpdateIconPosition();
+
+	// 위치 갱신을 담당할 타이머
+	FTimerHandle UpdateTimerHandle;
+
+	// 캐싱해둘 참조 변수들 (매번 찾으면 무거우니까)
+	TObjectPtr<APawn> CachedPlayerPawn;
+	TObjectPtr<AMGMiniMapCamera> CachedMinimapCamera;
+
+#pragma endregion
+
+};
