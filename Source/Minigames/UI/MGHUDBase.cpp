18de52d997c4175d56bf4ddbab5c65d581d81d65
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-07-13 18:28:04 +0900

    feat : MGHUDBase.h/cpp
    모든 미니게임 HUD의 부모 클래스 MGHUDBase에
    미니맵 위젯 추가

--- Source/Minigames/UI/MGHUDBase.cpp
+++ Source/Minigames/UI/MGHUDBase.cpp
@@ -1,4 +1,30 @@
-// MGHUDBase.cpp
+﻿// MGHUDBase.cpp
 
 #include "UI/MGHUDBase.h"
+#include "UI/MiniMap/UW_MiniMapLayout.h"
+#include "Minigames.h"
 
+void AMGHUDBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (GetNetMode() == NM_DedicatedServer)
+	{
+		return;
+	}	// 혹시 Dedicated Server에서 호출된다면 서버는 조기 return
+
+	if (!IsValid(MinimapWidgetClass))
+	{
+		return;
+	}
+
+	MinimapWidget = CreateWidget<UUW_MiniMapLayout>(GetOwningPlayerController(), MinimapWidgetClass);
+	if (IsValid(MinimapWidget))
+	{
+		MinimapWidget->AddToViewport();
+	}
+	else
+	{
+		MG_LOG_ROLE(LogMGNet, Log, TEXT("Fail to add Widget to Viewport"));
+	}
+}
