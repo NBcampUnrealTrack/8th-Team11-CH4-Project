5f753172178578762579642e23358d82cec8b646
runaway040722 <134586225+runaway040722@users.noreply.github.com>
2026-07-14 16:55:13 +0900

    pr

--- Source/Minigames/GameMode/MGLobbyGameModeBase.cpp
+++ Source/Minigames/GameMode/MGLobbyGameModeBase.cpp
@@ -235,7 +235,7 @@ void AMGLobbyGameModeBase::TravelToMinigameLevel()
 	}
 
 	GI->CurrentRoundState = ERoundState::Round1;
-	GetWorld()->ServerTravel("URL"); 
+	GetWorld()->ServerTravel(URL); 
 }
 
 void AMGLobbyGameModeBase::GenerateMinigameSequence()
