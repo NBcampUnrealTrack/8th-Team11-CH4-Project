c76e83b8fd149ef797927759f77bd1a353605189
runaway040722 <134586225+runaway040722@users.noreply.github.com>
2026-07-14 14:39:45 +0900

    title 추가

--- Source/Minigames/GameMode/MGLobbyGameModeBase.cpp
+++ Source/Minigames/GameMode/MGLobbyGameModeBase.cpp
@@ -235,7 +235,7 @@ void AMGLobbyGameModeBase::TravelToMinigameLevel()
 	}
 
 	GI->CurrentRoundState = ERoundState::Round1;
-	GetWorld()->ServerTravel(URL);
+	GetWorld()->ServerTravel("/Game/Minigames/Level/KTI"); // Test
 }
 
 void AMGLobbyGameModeBase::GenerateMinigameSequence()
