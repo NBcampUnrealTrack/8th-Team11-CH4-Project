90e2c2dfd279a5f6e2fd95b6ceaba1c262e32701
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-07-16 00:30:38 +0900

    refactor : MGMinimapTargetComponent.cpp
    미니맵에 표시할 액터의 EMinimapTargetType만 이용해서 미니맵에 등록하도록 책임 분리

--- Source/Minigames/Component/MGMinimapTargetComponent.cpp
+++ Source/Minigames/Component/MGMinimapTargetComponent.cpp
@@ -20,43 +20,49 @@ void UMGMinimapTargetComponent::BeginPlay()
 		return;
 	}
 
-	FTimerHandle RegisterTimer;
+	FTimerHandle RegisterTimerHandler;
 	GetWorld()->GetTimerManager().SetTimer(
-		RegisterTimer, 
+		RegisterTimerHandler, 
 		this, 
 		&UMGMinimapTargetComponent::RegisterToMinimap, 
-		0.2f, 
+		1.0f, 
 		false
 	);
 }
 
 void UMGMinimapTargetComponent::RegisterToMinimap()
 {
-	// 1. 나(컴포넌트)를 달고 있는 부모 액터를 가져옵니다.
 	AActor* OwnerActor = GetOwner();
-	if (!IsValid(OwnerActor)) return;
+	if (!IsValid(OwnerActor))
+	{
+		return;
+	}
 
-	// 2. 로컬 플레이어의 컨트롤러 및 HUD를 찾습니다.
 	APlayerController* LocalPC = UGameplayStatics::GetPlayerController(GetWorld(), 0);
-	if (!IsValid(LocalPC)) return;
+	if (!IsValid(LocalPC))
+	{
+		return;
+	}
 
 	AMGHUDBase* MyHUD = Cast<AMGHUDBase>(LocalPC->GetHUD());
-	if (!IsValid(MyHUD) || !IsValid(MyHUD->MinimapWidget)) return;
+	if (!IsValid(MyHUD))
+	{
+		return;
+	}
 
-	// 3. 기본적으로는 'Other(남/사물)' 아이콘을 세팅합니다.
-	TSubclassOf<UUserWidget> SelectedIconClass = OtherPlayerCharacterIcon;
+	if (!IsValid(MyHUD->MinimapWidget))
+	{
+		return;
+	}
 
-	// 4. 만약 부모 액터가 플레이어(Pawn)이고, 내 컴퓨터에서 조종 중이라면 'My' 아이콘으로 교체!
+	// 일단은 기본 Type::None으로 지정, 추후에 지정한 Type으로 Override
+	EMinimapTargetType FinalType = TargetType;
 	APawn* OwnerPawn = Cast<APawn>(OwnerActor);
 	if (IsValid(OwnerPawn) && OwnerPawn->IsLocallyControlled())
 	{
-		SelectedIconClass = MyPlayerCharacterIcon;
+		FinalType = EMinimapTargetType::My;		// 자신이 조종하고 있는 Pawn은 Type::My로
 	}
 
-	// 5. 미니맵 위젯에 최종적으로 부모 액터와 선택된 아이콘을 등록합니다.
-	if (IsValid(SelectedIconClass))
-	{
-		MyHUD->MinimapWidget->AddMinimapTarget(OwnerActor, SelectedIconClass);
-	}
+	MyHUD->MinimapWidget->AddMinimapTarget(OwnerActor, FinalType);	// 미니맵에 등록
 }
 
