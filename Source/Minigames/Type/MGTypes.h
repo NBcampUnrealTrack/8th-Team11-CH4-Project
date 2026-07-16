00ee721b906d499fcc5fdec93447ecdfc3eeb5fc
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-07-16 23:27:29 +0900

    refactor : MGTypes.h
    Entering -> PlayingCutScene -> Waiting -> Playing
    으로 순서 변경

--- Source/Minigames/Type/MGTypes.h
+++ Source/Minigames/Type/MGTypes.h
@@ -10,8 +10,8 @@ enum class EMatchState : uint8
 {
 	None,
 	Entering,			 // 심리스 트래블 후 클라이언트들이 맵에 들어오길 기다리는 상태
-	Waiting,			 // 모든 클라이언트가 Waiting 상태에 진입 시 Playing으로 전환하는 Timer 시작
 	PlayingCutScene,
+	Waiting,			 // 모든 클라이언트가 Waiting 상태에 진입 시 Playing으로 전환하는 Timer 시작
 	Playing,
 	RoundResult,		 // 해당 미니게임 결과 UI 표시 (게임 조작 불가, FInputModeUIOnly)
 	Ending,				 // ServerTravel을 호출하기 직전 마무리 단계
