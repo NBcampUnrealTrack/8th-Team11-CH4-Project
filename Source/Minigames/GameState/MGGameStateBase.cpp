afb1238a8541b6d065a0352a0958a811123c9912
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-07-17 23:49:08 +0900

    refactor : MGGameStateBase.cpp
    게임 설명 UI를 닫는 시점 변경

--- Source/Minigames/GameState/MGGameStateBase.cpp
+++ Source/Minigames/GameState/MGGameStateBase.cpp
@@ -57,7 +57,7 @@ void AMGGameStateBase::OnRep_MatchState()
 	// 게임이 시작(또는 그 이후)됐으면 인트로 숨김.
 	// PlayingCutScene/Playing만 보면, 폭탄게임처럼 시작과 동시에 Ending으로 넘어가는 경우
 	// 수동 OnRep 시점엔 이미 Ending이라 숨김을 놓침 → Entering/Waiting이 아니면 전부 숨김.
-	if (MatchState != EMatchState::Entering && MatchState != EMatchState::Waiting)
+	if (MatchState == EMatchState::PlayingCutScene || MatchState == EMatchState::Playing)
 	{
 		if (APlayerController* PC = GetGameInstance()->GetFirstLocalPlayerController())
 		{
