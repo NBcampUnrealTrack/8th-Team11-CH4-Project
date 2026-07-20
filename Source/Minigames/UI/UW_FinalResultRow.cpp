fb2bdbef91bd186c8bb81d3a5045181ecef097a3
YH760 <opqwoiwr@gmail.com>
2026-07-20 16:37:34 +0900

    hotfix: 라운드 결과창 문제 수정
    - 버튼게임 플레이 시간 늘림
    - 라운드 결과창 순위 색 제대로 뜨게 수정
    - FinalResult에서 라운드 결과창 안 뜨게 수정

--- Source/Minigames/UI/UW_FinalResultRow.cpp
+++ Source/Minigames/UI/UW_FinalResultRow.cpp
@@ -21,7 +21,7 @@ void UUW_FinalResultRow::Setup(AMGPlayerState* PS, int32 Rank)
 	case 1:  { BadgeColor = FLinearColor(1.00f, 0.84f, 0.00f); break; } // 금
 	case 2:  { BadgeColor = FLinearColor(0.75f, 0.75f, 0.78f); break; } // 은
 	case 3:  { BadgeColor = FLinearColor(0.80f, 0.50f, 0.20f); break; } // 동
-	default: { BadgeColor = FLinearColor(0.80f, 0.80f, 0.82f); break; } // 회색
+	default: { BadgeColor = FLinearColor(0.45f, 0.45f, 0.47f); break; } // 회색
 	}
 	RankBadge->SetColorAndOpacity(BadgeColor);
 	
