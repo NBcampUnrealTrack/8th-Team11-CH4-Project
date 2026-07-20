959ddf4b2e11eede12ea8db9d161d8d1b8106511
YH760 <opqwoiwr@gmail.com>
2026-07-20 15:25:46 +0900

    hotfix : GmaePlayTime
    - 게임 플레이 타임 조절

--- Source/Minigames/BTN/MGButtonGameModeBase.h
+++ Source/Minigames/BTN/MGButtonGameModeBase.h
@@ -31,7 +31,7 @@ protected:
     FTimerHandle GameTimerHandle;
 
     UPROPERTY(EditAnywhere, Category = "Game Settings")
-    int32 GameDuration = 30;
+    int32 GameDuration = 120;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Game Settings")
     int32 TimeRemaining;
